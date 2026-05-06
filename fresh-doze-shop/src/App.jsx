e888585241ac7b673e456d97c3365e2ea57878da
Oleg <oleg.rusnak04@gmail.com>
2026-05-06 16:51:27 +0300

    first commit

---
+++ fresh-doze-shop/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-red-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind працює!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
